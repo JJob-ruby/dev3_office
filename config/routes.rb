0e1868589e602824d506274ec91ceda3fb410b95
JJob <job.jagash@rubyians.com>
2020-01-16 14:32:38 +0530

    Added some routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   get 'welcome/index'
   devise_for :users
 
-  resources :profiles
+  resources :users
+  resources :tickets
+  resources :sprints
+  resources :projects
+  resources :ticket_time_logss
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
